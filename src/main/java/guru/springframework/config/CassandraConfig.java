918c9ce8804009b92fc348438f549c80e29180f9
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-04 21:40:49 +0300

    kmwork edit

---
+++ src/main/java/guru/springframework/config/CassandraConfig.java
@@ -0,0 +1,79 @@
+package guru.springframework.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jt on 10/6/17.
+ * edit Konstantin (kmwork)
+ */
+@Configuration
+@PropertySource(value = {"classpath:cassandra.properties"})
+@EnableCassandraRepositories(basePackages = "guru.springframework.repositories")
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    public static final String KEYSPACE = "guru_keyspace";
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+
+        return Arrays.asList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return KEYSPACE;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[]{"guru.springframework.domain"};
+    }
+
+    private static final Log LOGGER = LogFactory.getLog(CassandraConfig.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    @Bean
+    public CassandraClusterFactoryBean cluster() {
+        final CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+        cluster.setContactPoints(environment.getProperty("cassandra.contactpoints"));
+        cluster.setPort(Integer.parseInt(environment.getProperty("cassandra.port")));
+        LOGGER.info("Cluster created with contact points [" + environment.getProperty("cassandra.contactpoints") + "] " + "& port [" + Integer.parseInt(environment.getProperty("cassandra.port")) + "].");
+        return cluster;
+    }
+
+    @Override
+    @Bean
+    public CassandraMappingContext cassandraMapping() throws ClassNotFoundException {
+        return new CassandraMappingContext();
+    }
+}
